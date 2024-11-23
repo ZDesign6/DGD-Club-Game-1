399bddbe9ccc6c72d36019cd3e22d64584d23fd3
ZDesign6 <zd7business@gmail.com>
2024-11-23 16:33:19 -0500

    SonicMail and Songs functional
    Made the pipeline from Mail -> Song -> Instrument functional and created Test Assets from their Prefabs to prove system works. Note that audio playback may not work correctly, might need to add a delay in Song's play() fct.

--- Assets/Zach's Assets/Scripts/SongBehavior.cs
+++ Assets/Zach's Assets/Scripts/SongBehavior.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 public class SongBehavior : MonoBehaviour
 {
+    /*TO DO:
+    1) Figure out how to add in a wait period between sounds in playSong() */
+    
     /* This array contains the 4 Instruments that make
      * up this Song. This way we can reference a Song's
      * Instruments, their order, and all their Instrument data
      * whenever we need */
     public GameObject[] instrumentsInSong = new GameObject[4];
+
+    /*This fct iterates over this Song's instrumentsInSong,
+     * playing the audio file located in each of their 
+     * Audio Source Components */
+    public void playInstruments()
+    {
+        //iterate over Instruments in instrumentsInSong...
+        for (int songIndex = 0; songIndex < instrumentsInSong.Length; songIndex = songIndex + 1)
+        {
+            //play the Audio Resource of this Instrument's Audio Source Component
+            instrumentsInSong[songIndex].GetComponent<AudioSource>().Play();
+            //should probably add a waiting period before the loop recurs so we don't stack sounds
+        }
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Zach's Assets/Scripts/SonicMailBehavior.cs
+++ Assets/Zach's Assets/Scripts/SonicMailBehavior.cs
@@ -7,6 +7,13 @@ public class SonicMailBehavior : MonoBehaviour
     public GameObject song;
     //This is the List of Seeds that this Sonic Mail contains
     List<GameObject> seedsInMail = new List<GameObject>();
+    /*This fct calls the playSong() fct of this Mail's Song.
+     Called by Player Input 'Interact'.*/
+    public void playSong()
+    {
+        //Grab the specific instance of the Song in this Mail and use its PlayInstruments() method
+        song.GetComponent<SongBehavior>().playInstruments();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
